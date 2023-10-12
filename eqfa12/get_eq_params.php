28aa144ad6ea8aed335d2d1580af31a12ab6cf9a
lovehifi <81028393+lovehifi@users.noreply.github.com>
2023-10-12 09:36:25 +0700

    Update get_eq_params.php

--- eqfa12/get_eq_params.php
+++ eqfa12/get_eq_params.php
@@ -4,28 +4,29 @@ $configFilePath = '/etc/alsa/conf.d/eqfa12p.conf';
 
 // Hàm để trích xuất thông số EQ từ nội dung cấu hình
 function extractEQParams($configContent) {
-  $eqParameters = array();
+    $eqParameters = array();
 
-  // Tìm các mẫu 'controls [ ... ]'
-  preg_match('/controls \[(.*?)\]/', $configContent, $matches);
+    // Tìm các mẫu 'controls [ ... ]'
+    preg_match('/controls \[(.*?)\]/', $configContent, $matches);
 
-  // Lấy giá trị controls từ các mẫu tìm được
-  if (count($matches) > 1) {
-    $controls = preg_split('/\s+/', $matches[1], -1, PREG_SPLIT_NO_EMPTY);
+    // Lấy giá trị controls từ các mẫu tìm được
+    if (count($matches) > 1) {
+        $controls = preg_split('/\s+/', $matches[1], -1, PREG_SPLIT_NO_EMPTY);
 
-    // Lặp qua từng control và lấy giá trị gain cho 12 band
-    for ($i = 0; $i < count($controls); $i += 4) {
-      $band = ($i / 4) + 1;
-      $eqParameters['band' . $band] = array(
-        'enabled' => $controls[$i],
-        'freq' => $controls[$i + 1],
-        'q' => $controls[$i + 2],
-        'gain' => $controls[$i + 3]
-      );
+        // Lặp qua từng control và lấy giá trị gain cho 12 band
+        $numBands = min(count($controls) / 4, 12);  // Chỉ lấy 12 band
+        for ($i = 0; $i < $numBands * 4; $i += 4) {
+            $band = ($i / 4) + 1;
+            $eqParameters['band' . $band] = array(
+                'enabled' => $controls[$i],
+                'freq' => $controls[$i + 1],
+                'q' => $controls[$i + 2],
+                'gain' => $controls[$i + 3]
+            );
+        }
     }
-  }
 
-  return $eqParameters;
+    return $eqParameters;
 }
 
 // Đọc nội dung tệp cấu hình EQ
@@ -34,11 +35,21 @@ $configContent = file_get_contents($configFilePath);
 // Trích xuất thông số EQ từ nội dung cấu hình
 $eqParameters = extractEQParams($configContent);
 
-// Ghi thông số EQ và thông tin debug vào tệp JSON
-$jsonFilePath = '/srv/http/eq_params.json';
-file_put_contents($jsonFilePath, json_encode($eqParameters, JSON_PRETTY_PRINT));
+// Trích xuất giá trị volume từ chuỗi dữ liệu
+preg_match('/\bcontrols\s+\[.*\s+([0-9.-]+)\]/', $configContent, $volumeMatches);
+$volumeValue = isset($volumeMatches[1]) ? (float)$volumeMatches[1] : null;
 
-// Trả về thông số EQ dưới dạng JSON
+// Ghi thông số EQ và giá trị volume vào một đối tượng JSON
+$result = array(
+    'eqParams' => $eqParameters,
+    'volumeValue' => $volumeValue  // Đây là giá trị volume
+);
+
+// Ghi vào tệp eq_params.json
+$jsonFilePath = '/mnt/MPD/SD/eq_params.json';
+file_put_contents($jsonFilePath, json_encode($result, JSON_PRETTY_PRINT));
+
+// Trả về thông số EQ và giá trị volume dưới dạng JSON
 header('Content-Type: application/json');
-echo json_encode($eqParameters);
+echo json_encode($result);
 ?>
